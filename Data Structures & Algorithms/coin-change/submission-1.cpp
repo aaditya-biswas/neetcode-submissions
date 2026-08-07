4d7957e84a1a0241c7699e4cca63488a4feaf208
aaditya-biswas <147297053+aaditya-biswas@users.noreply.github.com>
2026-08-07 20:00:00 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1 ; i <=  amount ;i++) {
+            for (auto & m : coins) {
+                if (i - m < 0 || dp[i - m] == INT_MAX) continue;
+                dp[i] = min(dp[i] , 1 + dp[i-m]);
+            }
+        }
+        if (dp[amount] == INT_MAX) return -1;
+        return dp[amount];
+    }
+};
