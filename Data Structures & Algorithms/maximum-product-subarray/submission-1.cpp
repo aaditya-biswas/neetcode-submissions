17766b4309db7adfd58d3efe56a41301e44b3b05
aaditya-biswas <147297053+aaditya-biswas@users.noreply.github.com>
2026-08-07 21:52:01 +0530

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        // maxProduct 
+
+        // To solve this we can take two dps 
+        // One will calculate the maximum product  subarray 
+        // Second one will calculate the minimum product subarray
+        // For each index i we will do 
+        // max_product[i] = max({arr[i]  * max_product[i-1] , arr[i] * min_product[i-1] ,arr[i]} )
+        // min_product[i] = min({arr[i] * max_product[i-1], arr[i] * min_product[i-1] , arr[i]});
+        // Then output 
+        int n = nums.size();
+        vector<int> max_product(n ,INT_MIN);
+        vector<int> min_product(n , INT_MIN);
+        min_product[0] = nums[0];
+        max_product[0] = nums[0];
+        for (int i = 1; i < nums.size() ; ++i) {
+            max_product[i] = max({nums[i] , max_product[i-1] * nums[i],  min_product[i-1]  * nums[i]});
+            min_product[i] = min({nums[i], max_product[i-1] * nums[i], nums[i] * min_product[i-1] });
+            // Let the subarray with the max Proudct be k 
+        }
+        return *max_element(max_product.begin(), max_product.end());
+    }
+};
