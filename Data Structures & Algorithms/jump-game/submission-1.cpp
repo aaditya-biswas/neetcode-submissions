7707ca93e9524e36c7bc0a52b5b95aec4534f8ff
aaditya-biswas <147297053+aaditya-biswas@users.noreply.github.com>
2026-08-08 20:59:39 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,13 @@
+#pragma GCC optimize("O3,unroll-loops")
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size() ;
+        int farthest_ind = 0;
+        for (int i = 0 ; i <= min(farthest_ind, n -1 ) ; i++  ) {
+            farthest_ind = max(i + nums[i],farthest_ind); 
+        }        
+        return farthest_ind >= n-1;
+    }
+};
